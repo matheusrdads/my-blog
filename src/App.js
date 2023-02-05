0d62842321a0b04f2612d8846a4762caa042574c
matheusrdads <malbuquerquev8@gmail.com>
2023-02-04 21:41:57 -0300

    Crete links

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticleListPage from './pages/ArticlesListPage';
@@ -6,12 +7,19 @@ import ArticlePage from './pages/ArticlePage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Awesome Blog</h1>
-      <div id="page-body">
-        Welcome to my blog !
+    <BrowserRouter>
+      <div className="App">
+        <h1>My Awesome Blog</h1>
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage/>} />
+            <Route path="/articles" element={<ArticleListPage/>} />
+            <Route path="/articles/:articleId" element={<ArticlePage/>} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
