f558817bf686479ba4d7184c31dc2de80dc4210d
matheusrdads <malbuquerquev8@gmail.com>
2023-02-05 09:51:02 -0300

    Make articles list modular

---
+++ src/pages/article-content.js
@@ -0,0 +1,28 @@
+const articles = [
+  {
+    name: 'learn-react',
+    title: 'The Faster way to learning react',
+    content: [
+      'put her you React content'
+
+    ]
+  },
+  {
+    name: 'learn-node',
+    title: 'The Best way to learning node',
+    content: [
+      'put her you Node content'
+
+    ]
+  },
+  {
+    name: 'learn-mongodb',
+    title: 'The Funy way to learning Mongodb',
+    content: [
+      'put her you MongoDB content'
+
+    ]
+  }
+]
+
+export default articles
